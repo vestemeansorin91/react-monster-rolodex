38957637f75e0af1e13fc5a3fbf72329e25f9fbd
Sorin Vestemean <sorin.vestemean@edgeless.ro>
2021-03-20 17:02:30 +0200

    first page fix

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <h1>Monsters Rolodex</h1>
+        <h1 className='title'>Monsters Rolodex</h1>
         <SearchBox
           placeholder='Search monsters'
           handleChange={this.handleChange}
